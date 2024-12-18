eb373f04cddac2664099ea192f5fd98371e729fb
paras <parasjuly2003@gmail.com>
2024-12-18 19:00:43 +0530

    defining the roads , making road class , calculating the centers and the positions

--- main.js
+++ main.js
@@ -5,8 +5,9 @@ canvas.width = 200;
 // Get the 2D drawing context of the canvas
 const ctx = canvas.getContext("2d");
 
-// Create a new Car object with initial position and size
-const car = new Car(100, 100, 30, 50);
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
+
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);//getLaneCenter is a method of road class, it returns the x position of the lane
 
 animate();
 // Function to update and redraw the car
@@ -14,7 +15,15 @@ function animate() {
   // Update the car's position and state
   car.update();
   canvas.height = window.innerHeight; // Set the canvas height to the window's inner height to clear the canvas
+
+
+  ctx.save(); // Save the canvas context before clearing it
+  ctx.translate(0, -car.y + window.innerHeight / 2); // Translate the canvas to follow the car
+
+  road.draw(ctx); // Draw the road on the canvas
   car.draw(ctx); // Draw the car on the canvas
   // Request the next frame to keep the animation loop running
+
+  ctx.restore(); // Restore the canvas context after drawing the car
   requestAnimationFrame(animate); //this is a recursive function, calls the animate function again and again making it look like an animation
 }
---
+++ road.js
@@ -0,0 +1,55 @@
+class Road {
+  constructor(x, width, laneCount = 3) {
+    this.x = x;
+    this.width = width;
+    this.laneCount = laneCount;
+
+    this.left = x - width / 2;
+    this.right = x + width / 2;
+
+    const infinity = 1000000; //to avoid the car to go out of the screen with very large values , i.e very big path
+    this.top = -infinity;
+    this.bottom = infinity;
+
+    const topLeft = { x: this.left, y: this.top };
+    const topRight = { x: this.right, y: this.top };
+    const bottomLeft = { x: this.left, y: this.bottom };
+    const bottomRight = { x: this.right, y: this.bottom };
+    this.borders = [
+      [topLeft, bottomLeft],
+      [topRight, bottomRight],
+    ];
+  }
+
+  getLaneCenter(laneIndex) {
+    const laneWidth = this.width / this.laneCount;
+    return (
+      this.left +
+      laneWidth / 2 +
+      Math.min(laneIndex, this.laneCount - 1) * laneWidth
+    );
+  }
+
+  draw(ctx) {
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "white";
+
+    for (let i = 1; i <= this.laneCount - 1; i++) {
+      const x = this.left + (i * this.width) / this.laneCount; //calculating the x position of each lane, could have also used lerp(linear interpolation)
+
+      ctx.setLineDash([20, 20]);
+      ctx.beginPath(); //clears the current path in the canvas and begins a new path
+      ctx.moveTo(x, this.top); //moves the path to the specified point in the canvas, without creating a line
+      ctx.lineTo(x, this.bottom); //adds a new point and creates a line to that point from the last specified point in the canvas
+      ctx.stroke(); //draws the path you have defined with all those moveTo and lineTo methods
+    }
+
+    ctx.setLineDash([]);
+    this.borders.forEach((border) => {
+      ctx.beginPath();
+      ctx.moveTo(border[0].x, border[0].y);
+      ctx.lineTo(border[1].x, border[1].y);
+      ctx.stroke();
+    });
+  }
+}
